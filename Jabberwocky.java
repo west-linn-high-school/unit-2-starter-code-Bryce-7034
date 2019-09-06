e7807012acef5a47082d8125057086b8ccd8c119
Michael George <georgem@wlwv.k12.or.us>
2019-09-06 09:12:31 -0700

    Updated the Jabberwocky class to only have 1 print string

--- Jabberwocky.java
+++ Jabberwocky.java
@@ -1,10 +1,13 @@
+/**
+ * first stanza in the Jabberwocky poem
+ */
 public class Jabberwocky{
     public static void main(String[]args){
-        System.out.print("\n");
-        System.out.print("’twas brillig, and the slithy toves/\n");
-        System.out.print("	Did gyre and gimble in the wabe:\n");
-        System.out.print("All mimsy were the “borogroves”/\n");
-        System.out.print("	And the mome raths out grabe.\n");
-
+        System.out.print("\n"
+        +"’twas brillig, and the slithy toves/\n"
+        +"  Did gyre and gimble in the wabe:\n"
+        +"All mimsy were the \"borogroves\"/\n"
+        +"  And the mome raths out grabe.\n"
+        +"\n");
     }
 }
