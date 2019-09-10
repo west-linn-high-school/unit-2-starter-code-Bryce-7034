5605a411ee3c184d675187004e3ed3291610e9b6
Bryce-7034 <kellyb@wlhs.wlwv.k12.or.us>
2019-09-10 08:46:51 -0700

    updated and improved geometry class

--- Geometry.java
+++ Geometry.java
@@ -12,9 +12,10 @@ public class Geometry
         double radius = 5.0;
         final double PI = (3.14);
         
+        System.out.print("sides= "+sides+" radius= "+radius+" PI= "+PI+"\n");
         System.out.print("A heptagon has "+ sides+" sides\n");
-        System.out.print("A decagon has "+ (sides+3)+" sides\n");
-        System.out.print("A dodecagon has "+ (sides+radius)+" sides\n");
+        System.out.print("A decagon has "+ (sides+(10-sides))+" sides\n");
+        System.out.print("A dodecagon has "+ (sides+(12-sides))+" sides\n");
         System.out.print("The circumference of a circle with a radius of "
         + radius+" is "+(2*PI*radius));
     }
