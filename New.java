e88452c6d76e2d77dffe0386d790e6ef82c268c4
Bryce-7034 <kellyb@wlhs.wlwv.k12.or.us>
2019-09-12 09:42:06 -0700

    added "New" and changed "StringMutation" and "Geometry"

---
+++ New.java
@@ -0,0 +1,33 @@
+public class New{
+  public static void main(String[]args){
+    System.out.print("I hate BlueJ");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("\n");
+    System.out.print("                           ~\n");
+    System.out.print("Vision code more like bad [:)]\n");
+    System.out.print("                           ~");
+  }
+}
