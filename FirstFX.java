1f8e6216a0783d0176b90d4475c957d58a9ac6d5
Bryce-7034 <kellyb@wlhs.wlwv.k12.or.us>
2019-09-23 09:13:29 -0700

    added FirstFX

---
+++ FirstFX.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class FirstFX extends Application{
+  public void start(Stage stage){
+    
+  }
+}
